9b948967e01a69fb7cb4c20063aaa21e0f82f150
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-01 18:35:50 +0300

    Initial commit

--- src/Draft1_Task2_1_11.java
+++ src/Draft1_Task2_1_11.java
@@ -1,4 +1,4 @@
-public class Draft0_Whole_Video_2_1_11 {
+public class Draft1_Task2_1_11 {
     public static void main(String[] args) {
         System.out.println("Задание : \n1.\tРазбор домашнего задания 1.11\n\nРешение: ");
     }
